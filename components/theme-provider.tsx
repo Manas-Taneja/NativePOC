eadc36de257dddd2b9f79507f3e9e0ddf377cc95
Manas Taneja <manastanejaa@gmail.com>
2025-11-16 03:27:04 +0530

    Refactor ThemeProvider to use React.ComponentProps for improved type safety

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,13 +2,15 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
 
 /**
  * Theme Provider - Wraps the app to provide theme switching
  * Supports light, dark, and system theme with smooth transitions
  */
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({ 
+  children, 
+  ...props 
+}: React.ComponentProps<typeof NextThemesProvider>) {
   return (
     <NextThemesProvider
       attribute="class"
